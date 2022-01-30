ed2ae7df59971c70d0eb1d271aec2afc01f50a9f
Carlos Viera <pisis_13_3@hotmail.com>
2022-01-30 17:36:32 -0500

    Update App.js

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function (req, res) {
+app.get('/test/', function (req, res) {
+    console.log(req.query)
     const nom = req.query.nombre;
     res.send(`<h1>Hola ${nom}</h1>`)
 });
