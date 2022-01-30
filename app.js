bb6a78343a73d11d4975d772ec5534a25ccc54b2
Carlos Viera <pisis_13_3@hotmail.com>
2022-01-30 17:24:18 -0500

    add App.js

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+    const nom = req.query.nombre;
+    res.send(`<h1>Hola ${nom}</h1>`)
+});
+
+app.listen(3000, () => console.log('Listening on port 3000!'));
