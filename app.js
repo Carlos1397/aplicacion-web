04e6d02a30bcc0377d8f7bd93a96cf867a68cca4
Carlos Viera <pisis_13_3@hotmail.com>
2022-01-30 17:48:56 -0500

    Update App.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
-app.get('/', function (req, res) {
-    console.log(req.query)
-    const nom = req.query.nombre;
-    res.send(`<h1>Hola ${nom}!</h1>`)
+app.get('/', (req, res) => {
+    const { nombre = "desconocido" } = req.query;
+
+    res.send(`<h1>Hola ${nombre}!</h1>`);
 });
 
-app.listen(3000, () => console.log('Listening on port 3000!'));
+app.listen(3000, () => console.log('Listening on port 3000!'))
